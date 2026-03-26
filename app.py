2761aa124dfe0ae74473229d066ec613132cbd2a
Gabriel <gabrielalves.quadra@gmail.com>
2026-03-26 11:23:48 -0300

    feat: initial project setup and index route
    Initialize project structure
    Add basic configuration files (.gitignore)
    Install initial dependencies
    Create index route with respective template
    Create base layout for all future pages
    Add necessary images
    Create global css

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.context_processor
+def inject_now():
+    return dict(now=datetime.now())
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
